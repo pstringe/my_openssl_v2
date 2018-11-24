51463fda3c10c0d70dace0927137a05deff8de6a
Poitier Stringer <pstringe@e1z3r1p19.42.us.org>
2018-11-23 16:59:15 -0800

    initialized register constants

--- srcs/md5.c
+++ srcs/md5.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/19 09:19:39 by pstringe          #+#    #+#             */
-/*   Updated: 2018/11/19 12:24:19 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/11/23 16:58:35 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,11 @@
 
 void	md5(t_ssl *ssl)
 {
+	uint32_t	a0 = 0x67452301;
+	uint32_t	b0 = 0xefcdab89;
+	uint32_t 	c0 = 0x98badcfe;
+	uint32_t	d0 = 0x10325476;
+
+	
 	ft_printf("function evaluated:%s\n", ssl->expr->cmd.name);
 }
